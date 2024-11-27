22055dee6c62ff02c2d54fd522e8f756928c2b0e
131MohitYadav <yadavjimohit54@gmail.com>
2024-11-27 16:26:25 +0530

    bubble sort logic

---
+++ Array Programmes/ArraySort.java
@@ -0,0 +1,41 @@
+// Array Programmes
+
+// Write a java program to sort array in ascending order
+
+class ArraySort{
+
+public static void main(String args [ ]){
+
+int arr [ ]  = {30,20,10,40, 50};
+
+System.out.println("Before Sorting");
+
+for ( int i = 0; i< arr.length; i++){
+System.out.print("\t" + arr[i]);
+}
+
+// bubble sort logic
+
+for ( int i = 0; i< arr.length; i++){
+	
+	for ( int j = i + 1; j<arr.length; j++){
+		if ( arr [ i ] > arr [ j ]){
+int temp = arr [ i ] ; 
+arr [ i ] = arr [ j ];
+arr [ j ] = temp;
+}
+}
+}
+
+System.out.println(" \nAfter Sorting");
+
+for ( int i = 0; i< arr.length; i++){
+System.out.print("\t" + arr[i]);
+}
+
+}
+}
+
+
+
+
