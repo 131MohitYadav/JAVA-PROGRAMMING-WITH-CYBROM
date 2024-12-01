47866b7762126117b516f002b5fffa9cc742dd07
131MohitYadav <yadavjimohit54@gmail.com>
2024-12-01 21:39:14 +0530

    1 Dec 2024

---
+++ Array Programmes/ArrayRemove.java
@@ -0,0 +1,92 @@
+// Write a java program to remove duplicate element in any Array
+
+class ArrayRemove {
+    public static void main(String args[]) {
+        int arr[] = {10, 20, 20, 30, 30, 40, 50, 50}; 
+        int n = arr.length; // Length of the array
+
+        if (n == 0 || n == 1) { 
+            return n;
+        }
+
+        // Removing duplicates
+        int temp = 0; // Pointer for the position of unique elements
+        for (int i = 0; i < n - 1; i++) { 
+            if (arr[i] != arr[i + 1]) { // arr[0] = 10; arr[1] = 20
+                arr[temp++] = arr[i]; // 
+            }
+        }
+
+        arr[temp++] = arr[n - 1]; // Add the last unique element
+
+        // Printing the modified array
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < temp; i++) { // Print only up to the new length `j`
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
