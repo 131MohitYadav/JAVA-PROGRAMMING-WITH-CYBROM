f617e29c5a2ce6c5149efd569435d19c5ac71827
131MohitYadav <yadavjimohit54@gmail.com>
2024-12-08 20:03:52 +0530

    8 Dec 2024

---
+++ String Programmes/Indexof.java
@@ -0,0 +1,21 @@
+// String program
+
+// indexOf() method
+
+class Indexof{
+public static void main(String args [ ]){
+
+String s = "Hello Mohit Yadav";
+
+int i = s.indexOf("o");
+int j = s.indexOf("o");
+int k = s.indexOf("av");
+int m = s.indexOf("zee");
+
+System.out.println(i);//4
+System.out.println(j);
+System.out.println(k);// 15
+System.out.println(m);//-1
+
+}
+}
---
+++ String Programmes/LastIndexof.java
@@ -0,0 +1,21 @@
+// String program
+
+// lastIndexOf() method
+
+class LastIndexof{
+public static void main(String args [ ]){
+
+String s = "Hello Mohit Yadav";
+
+int i = s.lastIndexOf("o");
+int j = s.lastIndexOf("o");
+int k = s.lastIndexOf("av");
+int m = s.lastIndexOf("zee");
+
+System.out.println(i);//9
+System.out.println(j);//
+System.out.println(k);// 1
+System.out.println(m);//-1
+
+}
+}
