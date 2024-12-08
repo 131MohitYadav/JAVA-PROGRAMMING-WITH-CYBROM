e6a05e5735eb1dc40b093a290c5c4b208c526e72
131MohitYadav <yadavjimohit54@gmail.com>
2024-12-09 05:22:35 +0530

    9 Dec 2024

---
+++ String Programmes/Split.java
@@ -0,0 +1,16 @@
+// String program
+
+//split() method
+
+class Split{
+public static void main(String args [ ] ){
+
+String s = "Hello Mohit Yadav";
+String arr[ ] = s.split("o");
+System.out.println(arr[0]);
+System.out.println(arr[1]);
+System.out.println(arr[2]);
+System.out.println(arr[3]);
+
+}
+}
---
+++ String Programmes/Substring.java
@@ -0,0 +1,15 @@
+// String program
+// substring method
+
+class Substring{
+
+public static void main(String args [ ]){
+
+String s = "Welcome Mohit";
+String x = s.substring(3);
+String y = s.substring(6,9);
+System.out.println(x);
+System.out.println(y);
+
+}
+}
